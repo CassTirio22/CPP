4e75d732adfb915d57b3d3178949bd5e121cea78
CassTirio22 <cassandre.tirions@yahoo.be>
2022-03-20 18:18:37 +0100

    ex04 end

--- 01/ex04/replace.cpp
+++ 01/ex04/replace.cpp
@@ -6,7 +6,7 @@
 /*   By: ctirions <ctirions@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 16:51:24 by ctirions          #+#    #+#             */
-/*   Updated: 2022/03/19 17:27:33 by ctirions         ###   ########.fr       */
+/*   Updated: 2022/03/20 17:46:03 by ctirions         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,32 +16,38 @@ Replace::Replace(char *argv1, char *argv2, char *argv3) : _filename(argv1), _s1(
 Replace::~Replace(){}
 
 void	Replace::checkArgs(void){
-	if (!std::strncmp(this->_filename, "", 1) || this->_s1 == "" || this->_s2 == ""){
+	if (this->_filename == "" || this->_s1 == "" || this->_s2 == ""){
 		std::cout << "Args couldn't be empty ..." << std::endl;
 		exit (0);
 	}
 }
 
 std::string	Replace::openFilename(void){
-	std::string	text;
-	char		c;
-	FILE		*fin;
-
-	if (!(fin = std::fopen(this->_filename, "r+"))){
-		std::cout << "Use a valid filename ..." << std::endl;
+	char			c;
+	std::ifstream	fin(this->_filename);
+	
+	if (!fin.good())
 		exit (0);
-	}
-	while (std::fread(&c, sizeof(char), 1, fin)){
-		text.append(1, c);
-		if (std::feof(fin))
-			std::fclose(fin);
-	}
+	fin.seekg(0, std::ios::end);
+	size_t size = fin.tellg();
+	fin.seekg(0);
+	std::string	text(size, 'a');
+	fin.read(&text[0], size);
 	return (text);
 }
 
 void	Replace::makeReplaceFile(void){
 	std::string	text;
+	std::string	tmp;
+	size_t		pos = 0;
 
 	text = this->openFilename();
-	std::cout << text << std::endl;
+	std::ofstream	fout(this->_filename + ".replace");
+	while ((pos = text.find(this->_s1, pos)) != std::string::npos){
+		tmp = text.substr(0, pos);
+		text = tmp + this->_s2 + text.substr(pos + this->_s1.length(), text.length() - 1);
+		pos += this->_s2.length();
+	}
+	fout << text;
+	fout.close();
 }
--- 01/ex04/replace.hpp
+++ 01/ex04/replace.hpp
@@ -6,7 +6,7 @@
 /*   By: ctirions <ctirions@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 14:18:21 by ctirions          #+#    #+#             */
-/*   Updated: 2022/03/19 17:27:09 by ctirions         ###   ########.fr       */
+/*   Updated: 2022/03/20 17:14:01 by ctirions         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,15 @@
 
 # include <string>
 # include <iostream>
+# include <cstring>
+# include <fstream>
 
 class Replace
 {
 private:
 	std::string	_s1;
 	std::string	_s2;
-	const char	*_filename;
+	std::string	_filename;
 	
 	std::string	openFilename(void);
 
