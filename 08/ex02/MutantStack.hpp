439eabd551ad98e7288b72e8de1f6584bbb57c87
Cassandre Tirions <ctirions@fu-r7-p6.s19.be>
2022-05-18 18:45:42 +0200

    better 08/ex02

--- 08/ex02/MutantStack.hpp
+++ 08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: ctirions <ctirions@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 16:54:23 by ctirions          #+#    #+#             */
-/*   Updated: 2022/04/27 17:55:23 by ctirions         ###   ########.fr       */
+/*   Updated: 2022/05/18 18:41:46 by ctirions         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,12 @@ public:
 	}
 
 	typedef typename std::stack<T>::container_type::iterator iterator;
+	typedef typename std::stack<T>::container_type::reverse_iterator r_iterator;
 
 	iterator begin(void);
 	iterator end(void);
+	r_iterator rbegin(void);
+	r_iterator rend(void);
 };
 
 template <typename T>
@@ -43,6 +46,16 @@ typename MutantStack<T>::iterator	MutantStack<T>::begin(void){
 
 template <typename T>
 typename MutantStack<T>::iterator	MutantStack<T>::end(void){
+	return (this->c.end());
+}
+
+template <typename T>
+typename MutantStack<T>::r_iterator	MutantStack<T>::rbegin(void){
+	return (this->c.begin());
+}
+
+template <typename T>
+typename MutantStack<T>::r_iterator	MutantStack<T>::rend(void){
 	return (this->c.end());
 }
 
